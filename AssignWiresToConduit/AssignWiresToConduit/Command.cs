1c258f7d8c9703387042a4cf9ead3755ee27f4e7
Mr-Saxobeat <weiglasribeiro@gmail.com>
2019-04-26 16:26:40 -0300

    Add the first steps to get the conduit parameters
    Get all conduits and iterates with each of them, iterating with each parameter and getting the value of the gauge of each wiring.

--- AssignWiresToConduit/AssignWiresToConduit/Command.cs
+++ AssignWiresToConduit/AssignWiresToConduit/Command.cs
@@ -5,6 +5,7 @@ using System.Diagnostics;
 using Autodesk.Revit.ApplicationServices;
 using Autodesk.Revit.Attributes;
 using Autodesk.Revit.DB;
+using Autodesk.Revit.DB.Electrical;
 using Autodesk.Revit.UI;
 using Autodesk.Revit.UI.Selection;
 #endregion
@@ -19,39 +20,86 @@ namespace AssignWiresToConduit
           ref string message,
           ElementSet elements)
         {
+            //Get app and document from Revit
             UIApplication uiapp = commandData.Application;
             UIDocument uidoc = uiapp.ActiveUIDocument;
             Application app = uiapp.Application;
             Document doc = uidoc.Document;
 
-            // Access current selection
+            List<Conduit> listConduits = new List<Conduit>();
+            //Get all conduits from DB
+            #region GetConduits
+            FilteredElementCollector collector = new FilteredElementCollector(doc);
+            ICollection<Element> conduits = collector.OfClass(typeof(Conduit)).ToElements();
 
-            Selection sel = uidoc.Selection;
-
-            // Retrieve elements from database
-
-            FilteredElementCollector col
-              = new FilteredElementCollector(doc)
-                .WhereElementIsNotElementType()
-                .OfCategory(BuiltInCategory.INVALID)
-                .OfClass(typeof(Wall));
-
-            // Filtered element collector is iterable
-
-            foreach (Element e in col)
+            //Add conduits elements to listConduits
+            foreach (Conduit c in conduits)
             {
-                Debug.Print(e.Name);
+                listConduits.Add(c);
             }
+            #endregion
 
-            // Modify document within a transaction
+            //This dict remains the "wiring" as key and the gauge as value
+            Dictionary<string, double> wireGaugePairs = new Dictionary<string, double>();
 
-            using (Transaction tx = new Transaction(doc))
+            //Array of wiringParameters that will be get from conduits
+            string[] wiringParameters = new string[6];
+            #region Declare wiringParameters string names
+            wiringParameters[0] = "1.3. Fiação";
+            wiringParameters[1] = "2.3. Fiação";
+            wiringParameters[2] = "3.3. Fiação";
+            wiringParameters[3] = "4.3. Fiação";
+            wiringParameters[4] = "5.3. Fiação";
+            wiringParameters[5] = "6.3. Fiação";
+            #endregion
+
+            //Array of gaugeParameters that will be get from conduits
+            string[] gaugeParameters = new string[6];
+            #region Declare gaugeParameters string names
+            gaugeParameters[0] = "1.4. Bitola";
+            gaugeParameters[1] = "2.4. Bitola";
+            gaugeParameters[2] = "3.4. Bitola";
+            gaugeParameters[3] = "4.4. Bitola";
+            gaugeParameters[4] = "5.4. Bitola";
+            gaugeParameters[5] = "6.4. Bitola";
+            #endregion
+
+            //Iterates with each conduit
+            foreach (Conduit c in listConduits)
             {
-                tx.Start("Transaction Name");
-                tx.Commit();
+                //Iterates with each wiringParameters
+                for (int i = 0; i < 6; i++)
+                {
+                    Parameter wiringParam = c.LookupParameter(wiringParameters[i]);
+                    if (wiringParam.HasValue)
+                    {
+                        Parameter gaugeParam = c.LookupParameter(gaugeParameters[i]);
+                        if(gaugeParam.HasValue)
+                        {
+                            int nPhases = CountPhases(wiringParam.ToString());    
+                        }
+                    }
+                }
             }
 
             return Result.Succeeded;
+        }
+
+        //Return the number of phases of the 
+        //given wiring configurantion.
+        private int CountPhases(string wiring)
+        {
+            int nPhases = 0;
+
+            foreach (char c in wiring)
+            {
+                if (c.ToString() == "F")
+                {
+                    nPhases++;
+                }
+            }
+
+            return nPhases;
         }
     }
 }
